2c27a6f20e7e166089f9dc0a7b711fd36bdf0ed6
Patrick Pfeiffer <patrick@buzzle.at>
2012-02-26 05:16:27 +0100

    better tests

--- test/test.mocha.js
+++ test/test.mocha.js
@@ -1,3 +1,4 @@
+var ME = module.exports = {}
 var sv = require('../socketvat')
 var EE2 = require('eventemitter2').EventEmitter2
 var fs = require('fs')
@@ -13,120 +14,125 @@ var common =
     self.todo = todo
     self.did = function(e) {if (--self.todo<=0) cb && cb(e)}
   }
-, scenario: function(opts,done) {
-    //console.log(opts)
-    var events = Object.keys(opts.events)
-    var p = common.plan(0,done)
-    events.forEach(function(i){
-      Object.keys(opts.events[i]).forEach(function(x){
-        p.todo = p.todo+2
-        //common.clientVat.onAny(common.ee2log('clientVat **'))
-        common.clientVat.once(x,function(d){
-          assert.deepEqual([].slice.call(arguments),opts.events[i][x])
-          p.did()
-        })
-        //common.serverVat.onAny(common.ee2log('serverVat **'))
-        common.serverVat.once(x,function(){
-          assert.deepEqual([].slice.call(arguments),opts.events[i][x])
-          p.did()
-        })
-      })
-    })
-    common.serverRemotes[0].remote[opts.method].apply(common.serverRemotes[0].remote,opts.args)
-    common.clientRemotes[0].remote[opts.method].apply(common.clientRemotes[0].remote,opts.args)
-  }
 }
+       
+var remoteSamples = 
+[ { name:'set'  , methods:[['set','foo','bar']] , events:[['set','foo','bar']]    }
+, { name:'get'  , methods:[['get','foo']]       , events:[['get','foo','bar']]    }
+, { name:'keys' , methods:[['keys','.*']]       , events:[['keys',['foo'],/.*/]] }
+]
 
-module.exports =
-{ remote: 
-  { before: function(done){
-      var p = common.plan(2,done)
-      var port = ~~(Math.random()*50000)+10000
-      common.serverVat = sv()
-      //common.serverVat.onAny(common.ee2log('serverVatAny'))
-      common.serverRemotes = []
-      common.serverVat.listen(port,function(rem,s){
-        common.serverRemotes.push({remote:rem,socket:s})
+ME.remote = {}
+ME.remote.before = function(done){
+  var p = common.plan(2,done)
+  var port = ~~(Math.random()*50000)+10000
+  common.serverVat = sv()
+  //common.serverVat.onAny(common.ee2log('serverVatAny'))
+  common.serverRemotes = []
+  common.serverVat.listen(port,function(rem,s){
+    common.serverRemotes.push({remote:rem,socket:s})
+    p.did()
+  })
+  common.clientVat = sv()
+  //common.clientVat.onAny(common.ee2log('clientVatAny'))
+  common.clientRemotes = []
+  common.clientVat.connect(port,function(rem,s){
+    common.clientRemotes.push({remote:rem,socket:s})
+    p.did()
+  })
+}     
+
+remoteSamples.forEach(function(x){
+  ME.remote[x.name] = function(done){
+    var p = common.plan(0,done)
+    x.methods.forEach(function(m){
+      var currM = m.shift()
+      common.serverRemotes[0].remote[currM]
+        .apply(common.serverRemotes[0].remote,m)
+      common.clientRemotes[0].remote[currM]
+        .apply(common.clientRemotes[0].remote,m)
+    })
+    x.events.forEach(function(e){
+      p.todo = p.todo+2
+      var currE = e.shift()
+      common.clientVat.once(currE,function(){
+        assert.deepEqual([].slice.call(arguments),e)
         p.did()
       })
-      common.clientVat = sv()
-      //common.clientVat.onAny(common.ee2log('clientVatAny'))
-      common.clientRemotes = []
-      common.clientVat.connect(port,function(rem,s){
-        common.clientRemotes.push({remote:rem,socket:s})
+      common.serverVat.once(currE,function(){
+        assert.deepEqual([].slice.call(arguments),e)
         p.did()
       })
-    }
-  , set:  function(done){common.scenario({method:'set',args:['foo','bar'],events:[{'set':['foo','bar']}]},done)}
-  , get:  function(done){common.scenario({method:'get',args:['foo'],events:[{'get':['foo','bar']}]},done)}
-  , keys: function(done){common.scenario({method:'keys',args:['.*'],events:[{'keys':[['foo'],/.*/]}]},done)}
+    })
   }
-, 'simple set/get': function(done){
-    var p = common.plan(10,done)
-    var port = ~~(Math.random()*50000)+10000
-    var serverVat = sv()
-    serverVat.listen(port,function(r){
-      r.on('*',function(){
-        assert.equal(this.event[0],'data')
-        assert.equal(this.event[1],'socketvat')
-        assert.equal(this.event[2],'event')
-        var args = [].slice.call(arguments)
-        assert.deepEqual(args,['server','x'])
-        if (this.event[3] == 'set') p.did()
-        if (this.event[3] == 'get') p.did()
-      })
-      r.set('server','x') 
-      r.get('server')   
-    })                              
-    
-    var clientVat = sv()
-    clientVat.connect(port,function(r){
-      r.on('*',function(){
-        assert.equal(this.event[0],'data')
-        assert.equal(this.event[1],'socketvat')
-        assert.equal(this.event[2],'event')
-        var args = [].slice.call(arguments)
-        assert.deepEqual(args,['client','y'])
-        if (this.event[3] == 'set') p.did()
-        if (this.event[3] == 'get') p.did()
-      })
-      r.set('client','y') 
-      r.get('client')
+})
+  
+ME['simple set/get'] = function(done){
+  var p = common.plan(10,done)
+  var port = ~~(Math.random()*50000)+10000
+  var serverVat = sv()
+  serverVat.listen(port,function(r){
+    r.on('*',function(){
+      assert.equal(this.event[0],'data')
+      assert.equal(this.event[1],'socketvat')
+      assert.equal(this.event[2],'event')
+      var args = [].slice.call(arguments)
+      assert.deepEqual(args,['server','x'])
+      if (this.event[3] == 'set') p.did()
+      if (this.event[3] == 'get') p.did()
     })
-    
-    serverVat.on('*',function(){
-      if (this.event == 'set' && arguments[0] == 'foo'    && arguments[1] == 'bar') p.did()
-      if (this.event == 'set' && arguments[0] == 'client' && arguments[1] == 'y')   p.did()
-      if (this.event == 'get' && arguments[0] == 'client' && arguments[1] == 'y')   p.did()
+    r.set('server','x') 
+    r.get('server')   
+  })                              
+  
+  var clientVat = sv()
+  clientVat.connect(port,function(r){
+    r.on('*',function(){
+      assert.equal(this.event[0],'data')
+      assert.equal(this.event[1],'socketvat')
+      assert.equal(this.event[2],'event')
+      var args = [].slice.call(arguments)
+      assert.deepEqual(args,['client','y'])
+      if (this.event[3] == 'set') p.did()
+      if (this.event[3] == 'get') p.did()
     })
-    clientVat.on('*',function(){
-      if (this.event == 'set' && arguments[0] == 'foo'    && arguments[1] == 'bar') p.did()
-      if (this.event == 'set' && arguments[0] == 'server' && arguments[1] == 'x')   p.did()
-      if (this.event == 'get' && arguments[0] == 'server' && arguments[1] == 'x')   p.did()
-    })
-    
-    serverVat.set('foo','bar')
-    clientVat.set('foo','bar') 
-  }
-, tls: function(done) {
-    var p = common.plan(2,done)
-    var port = ~~(Math.random()*50000)+10000
-    var fixturesPath = path.resolve(__dirname+'/../node_modules/nssocket/test/fixtures')
-    var opts = { port : port
-               , key  : fs.readFileSync(fixturesPath+'/ryans-key.pem')
-               , cert : fs.readFileSync(fixturesPath+'/ryans-cert.pem')
-               , ca   : fs.readFileSync(fixturesPath+'/ryans-csr.pem')
-               }
-    var serverVat = sv()
-    serverVat.listen(opts,function(rem,s){
-      assert.equal(s._type,'tls')
-      p.did()
-    })
-    var clientVat = sv()
-    clientVat.connect(opts,function(rem,s){
-      assert.equal(s._type,'tls')
-      p.did()
-    }) 
-  }
+    r.set('client','y') 
+    r.get('client')
+  })
+  
+  serverVat.on('*',function(){
+    if (this.event == 'set' && arguments[0] == 'foo'    && arguments[1] == 'bar') p.did()
+    if (this.event == 'set' && arguments[0] == 'client' && arguments[1] == 'y')   p.did()
+    if (this.event == 'get' && arguments[0] == 'client' && arguments[1] == 'y')   p.did()
+  })
+  clientVat.on('*',function(){
+    if (this.event == 'set' && arguments[0] == 'foo'    && arguments[1] == 'bar') p.did()
+    if (this.event == 'set' && arguments[0] == 'server' && arguments[1] == 'x')   p.did()
+    if (this.event == 'get' && arguments[0] == 'server' && arguments[1] == 'x')   p.did()
+  })
+  
+  serverVat.set('foo','bar')
+  clientVat.set('foo','bar') 
+}
+  
+ME.tls = function(done) {
+  var p = common.plan(2,done)
+  var port = ~~(Math.random()*50000)+10000
+  var fixturesPath = path.resolve(__dirname+'/../node_modules/nssocket/test/fixtures')
+  var opts = { port : port
+             , key  : fs.readFileSync(fixturesPath+'/ryans-key.pem')
+             , cert : fs.readFileSync(fixturesPath+'/ryans-cert.pem')
+             , ca   : fs.readFileSync(fixturesPath+'/ryans-csr.pem')
+             }
+  var serverVat = sv()
+  serverVat.listen(opts,function(rem,s){
+    assert.equal(s._type,'tls')
+    p.did()
+  })
+  var clientVat = sv()
+  clientVat.connect(opts,function(rem,s){
+    assert.equal(s._type,'tls')
+    p.did()
+  }) 
 }
 
